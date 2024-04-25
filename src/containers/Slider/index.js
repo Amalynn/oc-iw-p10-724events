c077d6814cb0858f9992d1cbbdb8870fe87d19fe
Amalynn <amandine.ebner@gmail.com>
2024-04-25 11:37:13 +0200

    Bugs fixing - Slider : removing React fragment + improving key uniqueness

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,11 +8,14 @@ const Slider = () => {
 
   const { data } = useData();  
   const [index, setIndex] = useState(0);
+  console.log(data);
 
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-      new Date(evtB.date) - new Date(evtA.date)
-  );    
+      new Date(evtB.date) - new Date(evtA.date));
+  
+      
 
+  console.log(byDateDesc);
   const nextCard = () => {
     setTimeout(
       () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
@@ -26,37 +29,35 @@ const Slider = () => {
   
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+      {byDateDesc.map((event, idx) => (        
+        <div
+          key={`focus_${(event.title).replaceAll(" ", "_")}_${getMonth(new Date(event.date))}_${(new Date(event.date)).getFullYear()}`}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((event, radioIdx) => (
+            <input
+              key={`input_${(event.title).replaceAll(" ", "_")}_${getMonth(new Date(event.date))}_${(new Date(event.date)).getFullYear()}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+            />
+          ))}
+        </div>
+      </div>      
     </div>
   );
 };
